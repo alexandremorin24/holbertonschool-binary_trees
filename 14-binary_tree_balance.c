bcb9d51f8f2574aa3010999120f3057b21522987
Alexandre <9761@holbertonstudents.com>
2024-12-10 14:07:53 +0100

    Task 14 last fix ?

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,18 +6,15 @@
  *
  * Return: Height of the tree, or 0 if tree is NULL
  */
-size_t binary_tree_height_child(const binary_tree_t *tree)
+int binary_tree_height_child(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	left_height = binary_trebinary_tree_height_childe_height(tree->left);
-	right_height = binary_trebinary_tree_height_childe_height(tree->right);
+	left_height = binary_tree_height_child(tree->left);
+	right_height = binary_tree_height_child(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 
@@ -36,11 +33,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left_height = binary_tree_height_child(tree->left);
+	left_height = binary_tree_height_child(tree->left);
 
-	if (tree->right != NULL)
-		right_height = binary_tree_height_child(tree->right);
+	right_height = binary_tree_height_child(tree->right);
 
 	return (left_height - right_height);
 }
